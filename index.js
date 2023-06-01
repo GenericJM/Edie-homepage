0d01da15d6ca72bf71dd939ba3677c8ecfaee6d7
GenericJM <juan.arturo.m.m@gmail.com>
2023-06-01 13:39:27 -0400

    add links into the page and style to nav-bar

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 "use strict"
 const nav= document.querySelector(".nav");
 const bars=document.querySelector(".material-symbols-outlined");
+const main=document.querySelector(".main");
 
 bars.addEventListener("click",()=>{
     if(nav.classList.contains("nav-active")){
-      nav.classList.remove("nav-active");
-    //   bars.classList.add("material-symbols-outlined-active")
+      nav.classList.remove("nav-active"); 
     }
     else{
         nav.classList.add("nav-active");
     }
+})
+main.addEventListener("click",()=>{
+  if(!nav.classList.contains("nav-active")){    
+    nav.classList.add("nav-active");
+    
+  }
 })
